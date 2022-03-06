d81c204842e203d6e8e1c2c9b859cf982a4dea97
nsilva90 <nikolas.silva1990@gmail.com>
2022-03-06 15:41:38 -0300

    Importação dos dados do arquivo .CSV para a tabela 'dados_ecommerce'

--- SQL_ScriptDB.sql
+++ SQL_ScriptDB.sql
@@ -1,6 +1,8 @@
+-- Criação do banco de dados no servidor:
 create database bagy;
 use bagy;
 
+-- Criação da tabela no banco de dados 'bagy':
 create table dados_ecommerce
 (
 	InvoiceNo 	varchar(100) null, 
@@ -14,4 +16,21 @@ create table dados_ecommerce
 	Country		varchar(100) null
 );
 
+-- Importação dos dados do arquivo .CSV para a tabela 'dados_ecommerce':
+LOAD DATA LOCAL INFILE '/home/nikolas/Bagy/teste_dados_ecommerce.csv'
+INTO TABLE dados_ecommerce
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(InvoiceNo, StoreId, StockCode, Description, Quantity, InvoiceDate, UnitPrice, CustomerID, Country);
+
+-- Validação da quantidade de registros importados:
+select count(*) from dados_ecommerce; -- 541909 registros ok
+
+-- Validação de exibição dos dados:
 select * from dados_ecommerce;
+
+
+
+
