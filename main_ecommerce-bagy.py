cdecf6586a79fd9cf81326b2413aec4ee91c02a7
nsilva90 <nikolas.silva1990@gmail.com>
2022-03-06 17:16:57 -0300

    Desafio 2 concluído

--- main_ecommerce-bagy.py
+++ main_ecommerce-bagy.py
@@ -31,6 +31,45 @@ for line in cursor.fetchall():
     print(f"StoreId: {line[0]}, Volume total de itens vendidos: {line[1]}, Valor total vendido: {line[2]}")
 
 
+# Desafio 2
+
+print("\n"
+"--------------------------------------------------------------------------------------------\n"
+"\n"
+"2.1) Qual ticket médio mensal dos pedidos apresentados?\n"
+"\n"
+"Resposta:")
+
+cursor.execute(
+    "select date_format(data, '%M') mes, AVG(TotalValue) InvoiceValues "
+    "from "
+    "(select str_to_date(InvoiceDate, '%m/%d/%Y') data, "
+    "InvoiceNo, "
+    "SUM(Quantity * UnitPrice) TotalValue "
+    "from dados_ecommerce "
+    "group by data, InvoiceNo) data_trunced "
+    "group by mes "
+    "order by InvoiceValues desc "
+)
+
+for line in cursor.fetchall():
+    print(f"Mês: {line[0]}, Média de Valor dos Pedidos: {line[1]}")
+
+print("\n"
+"2.2) Qual o volume médio mensal de vendas (todas as lojas)?\n"
+"\n"
+"Resposta:")
+
+cursor.execute(
+    "select date_format(str_to_date(InvoiceDate, '%m/%d/%Y'), '%M') mes, "
+    "AVG(Quantity) media "
+    "from dados_ecommerce "
+    "group by mes "
+    "order by media desc "
+)
+
+for line in cursor.fetchall():
+    print(f"Mês: {line[0]}, Média de vendas: {line[1]}")
 
 
 
