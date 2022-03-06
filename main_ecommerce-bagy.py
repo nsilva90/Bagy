3388519a4e66fcbdf97652de9f646de9d97376a4
nsilva90 <nikolas.silva1990@gmail.com>
2022-03-06 16:58:39 -0300

    Desafio 1 concluído

--- main_ecommerce-bagy.py
+++ main_ecommerce-bagy.py
@@ -11,5 +11,31 @@ conn = mysql.connector.connect(
 
 cursor = conn.cursor()
 
+# Desafio 1
+
+print("1) Quais as 10 lojas com maior faturamento em pedidos? Sumarize os dados dessa loja apresentando os seguintes campos:\n"
+"- StoreId\n"
+"- Volume total de itens vendidos\n"
+"- Valor total vendido\n"
+"\n"
+"Resposta:")
+
+cursor.execute(
+    'select StoreId, SUM(Quantity) Quantity, SUM(UnitPrice) Billing '
+    'from dados_ecommerce '
+    'group by StoreId '
+    'order by sum(UnitPrice) desc limit 10 '
+)
+
+for line in cursor.fetchall():
+    print(f"StoreId: {line[0]}, Volume total de itens vendidos: {line[1]}, Valor total vendido: {line[2]}")
+
+
+
+
+
+
+
+
 
 
